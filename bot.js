2e4f042020827e18f9fe53b4d9a705786e590833
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-26 14:15:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,18 @@ client.on('message', message => {
   setInterval(lol, 5000);
 });
 
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '527458463777947658').setName("❤ W");
+client.channels.find('id', '527458463777947658').setName("We");
+client.channels.find('id', '527458463777947658').setName("Wel");
+client.channels.find('id', '527458463777947658').setName("Welc");
+client.channels.find('id', '527458463777947658').setName("Welco");
+client.channels.find('id', '527458463777947658').setName("Welcom");
+client.channels.find('id', '527458463777947658').setName("Welcome");
+client.channels.find('id', '527458463777947658').setName("Welcome❤ ");
+  }, 3000);
+});
 
 		 
 client.login(process.env.BOT_TOKEN);
