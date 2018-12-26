0e8b7c497e3e2f2d4eff15b5a431636458cbd043
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-26 14:28:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,24 +26,5 @@ client.on('message', message => {
   setInterval(lol, 5000);
 });
 
-cclient.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', '527458463777947658').setName("W");
-client.channels.find('id', '527458463777947658').setName("We");
-client.channels.find('id', '527458463777947658').setName("Wel");
-client.channels.find('id', '527458463777947658').setName("Welc");
-client.channels.find('id', '527458463777947658').setName("Welco");
-client.channels.find('id', '527458463777947658').setName("Welcom");
-client.channels.find('id', '527458463777947658').setName("Welcome");
-client.channels.find('id', '527458463777947658').setName("Welcome T");
-client.channels.find('id', '527458463777947658').setName("Welcome To");
-client.channels.find('id', '527458463777947658').setName("Welcome To C");
-client.channels.find('id', '527458463777947658').setName("Welcome To Co");
-client.channels.find('id', '527458463777947658').setName("Welcome To Cod");
-client.channels.find('id', '527458463777947658').setName("Welcome To Code");
-client.channels.find('id', '527458463777947658').setName("Welcome To Codes♥");
-  }, 3000);
-});
-
 		 
 client.login(process.env.BOT_TOKEN);
