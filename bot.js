70a0bcb462f6248205b0baa7586d1db268d78f09
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-26 14:53:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,13 +19,4 @@ client.on('message', message => {
   }
 });
 
-	client.on("ready", () => { // كود رينبو
-  function lol() {
-    client.guilds.get('471121072591470604').roles.find("name", "Rainbow").setColor("RANDOM");
-  };
-  setInterval(lol, 5000);
-});
-
-
-		 
 client.login(process.env.BOT_TOKEN);
